174a47cffcbdb546a1ba387b6da570535a1cb738
Shreyas Kumar <shreyaskr.0914@gmail.com>
2026-04-15 17:07:16 +0530

    Added .env (no important information inside) for future configuration

--- pi_vid_stream/config.py
+++ pi_vid_stream/config.py
@@ -1,9 +1,19 @@
-# Configuration settings for the Edge Node
-CAMERA_INDEX = 0
-STREAM_PORT = 5000
-JPEG_QUALITY = 70
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_int(key, default):
+    return int(os.getenv(key, default))
+
+
+CAMERA_INDEX = get_int("CAMERA_INDEX", 0)
+STREAM_PORT = get_int("STREAM_PORT", 5000)
+JPEG_QUALITY = get_int("JPEG_QUALITY", 70)
 
 # Motion Gate Settings
-MOTION_HISTORY = 500
-MOTION_VAR_THRESHOLD = 50
-MOTION_PIXEL_THRESHOLD = 1500  # Lower this to make it more sensitive
+MOTION_HISTORY = get_int("MOTION_HISTORY", 500)
+MOTION_VAR_THRESHOLD = get_int("MOTION_VAR_THRESHOLD", 50)
+MOTION_PIXEL_THRESHOLD = get_int("MOTION_PIXEL_THRESHOLD", 1500)
